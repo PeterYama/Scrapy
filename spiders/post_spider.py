700424be120cb81c1adc5c9547c0920d49048f4e
PeterYama <35582343+PeterYama@users.noreply.github.com>
2020-08-06 09:45:07 +1000

    added comments

--- spiders/post_spider.py
+++ spiders/post_spider.py
@@ -5,7 +5,21 @@ class PostSpider(scrapy.Spider):
     start_urls = [
         'https://www.siteinspire.com/',
     ]
+    #Passes the URL of each post
+    def parse(self, response):
+        for post in response.css('div.thumbnail'):
+            url = post.css('.wrapper .image a::attr(href)').get()
 
+            if url is not None:
+                url = response.urljoin(url)
+                yield response.follow(url, callback=self.fillCategory)
+        #After reading the first page, navigate to the next Page
+        next_page = response.css('.pagination .next::attr(href)').get()
+        if next_page is not None: 
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+
+    # Custom method classthat access each post
     def fillCategory(self, response):
         if response.css('[id="main"]') is not None:
             for item in response.css('[id="main"]'):
@@ -14,19 +28,7 @@ class PostSpider(scrapy.Spider):
                     'Site-URL' : response.xpath('//*[@id="website"]/div[2]/div/ul[1]/li[1]/a/span[2]/text()').extract_first(),
                     'Categories': item.css('.context ul li a::text')[0].get()
                 }
-
-    def parse(self, response):
-        for post in response.css('div.thumbnail'):
-            url = post.css('.wrapper .image a::attr(href)').get()
-            if url is not None:
-                url = response.urljoin(url)
-                yield response.follow(url, callback=self.fillCategory)
-
-        next_page = response.css('.pagination .next::attr(href)').get()
-        print("NUMBER FOUND ON NEXT PAGE : "+ str(next_page.find('9')))
-        if next_page is not None: 
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+            
 
             
         
